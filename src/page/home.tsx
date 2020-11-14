c7e177a2a5c8ae88f3076bed3635c43f91b1e955
Felix <13115412392@163.com>
2020-11-14 14:30:23 +0800

    feat:init

---
+++ src/page/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'antd';
+import './home.css';
+
+const Home = () => (
+  <div className="home-container">
+    
+  </div>
+);
+
+export default Home;
